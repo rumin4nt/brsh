34a1c8ec65ddd6dc720815a7e478c9b240a55149
vs <andrew@vaporstack.org>
2018-04-02 14:49:33 -0400

    lua brush stub

---
+++ data/brushes/sample_brush_lua.lua
@@ -0,0 +1,22 @@
+
+--	simple as can be.
+--	the wash host application provides arrays of fixed lengths:
+--	points_x
+--	points_y
+--	pressure
+--	rotation
+--	tilt_x
+--	tilt_y
+
+--	and some others TBD
+--	probably will provide seeded randoms as well and maybe noise
+--
+
+--	probably will add some better type checking and safety later too, idk!
+
+--	define a single function, render, that takes data and returns a stroke
+
+function render(data)
+
+end
+
