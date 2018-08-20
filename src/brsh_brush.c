c417358efeb0edd1ef845deac35f3c0f6cecbbc2
vaporstack <andrew@vaporstack.org>
2018-08-20 13:41:42 -0400

    brsh tweaks

--- src/brsh_brush.c
+++ src/brsh_brush.c
@@ -41,27 +41,27 @@ BBrush* brsh_brush_create(struct WLineHnd ref, double width)
 
 BBrush* brsh_brush_copy(struct BBrush* old, struct WLineHnd hnd)
 {
-
+	
 	if (old == NULL)
 	{
 		printf("tried to copy a null brush\n");
 		return NULL;
 	}
-
+	
 	// BBrush* old = src->brush;
 	if (!old)
 		return NULL;
-
+	
 	BBrush* brush       = brsh_brush_create(hnd, old->width);
 	brush->needs_update = old->needs_update;
-
+	
 	if (old->stroke)
 	{
 		// brush->stroke = calloc(1, sizeof(WLine));
 		brush->stroke = wsh_line_copy(old->stroke);
 		// memcpy(brush->stroke, old->stroke, sizeof(WLine));
 	}
-
+	
 	brush->hnd = hnd;
 	printf("Copied a brush for line %p\n", old);
 #ifdef DISABLE_UNTIL_WORKLINE_REFACTOR_COMPLETE
@@ -92,9 +92,9 @@ void brsh_brush_destroy(BBrush* brush)
  float dy = y2 - y1;
  float angle = atan2(dy,dx);
  //return atan2(dy, dx);
-
-
-
+ 
+ 
+ 
  if (dy < 0 ) {
  angle += 2 * (float)M_PI;
  }
@@ -104,22 +104,22 @@ void brsh_brush_destroy(BBrush* brush)
  angle -= 270;
  return angle;
  }
-
-
+ 
+ 
  static inline float angle_from_points_wp(WPoint a, WPoint b)
  {
  return angle_from_points(a.x,a.y, b.x,b.y);
  }
-
+ 
  */
 
 /*
-
+ 
  static inline float deg2rad(float input){
-
+ 
  return M_PI * input / 180 ;
  }
-
+ 
  */
 
 //#define mult .125
@@ -137,27 +137,26 @@ void brsh_brush_update(BBrush* brush, brush_update_func func)
 		return;
 	}
 	//if ( brush->hnd.src )
-		
-		
+	
+	
 	//	if we pass a function directly to this function, can assume we are overriding.
 	if (func)
 	{
-
+		
 		func(brush);
 		return;
 	}
-
+	
 	if (brush->update_func)
 	{
 		brush_update_func f = (brush_update_func)brush->update_func;
-
 		f(brush);
 	}
 	else
 	{
-
+		
 		//f(brush);
-
+		
 		// brsh_brush_update_new_slow( brush);
 		brsh_brush_update_old_fast(brush);
 	}
@@ -171,13 +170,13 @@ void brsh_brush_offset(struct BBrush* brush)
 static void _brush_tangents(BBrush* brush, WPoint a, WPoint b, double* lx,
 			    double* ly, double* rx, double* ry)
 {
-
+	
 	double ang = deg2rad(angle_from_points_wp(a, b));
 	double ps  = a.pressure;
-
+	
 	*lx = a.x - (ps * cos(ang) * brush->width);
 	*ly = a.y - (ps * sin(ang) * brush->width);
-
+	
 	*rx = b.x + (ps * cos(ang) * brush->width);
 	*ry = b.y + (ps * sin(ang) * brush->width);
 }
@@ -190,15 +189,15 @@ void brsh_brush_update_new_slow(BBrush* brush)
 		//		oops
 		return;
 	}
-
+	
 	// printf("Updating brush!\n");
-
+	
 	double nx, ny;
 	double px, py;
 	double x, y;
 	px = py = x = y = nx = ny = 0;
 	WLine* src		  = brush->hnd.src;
-
+	
 	WLine* cpy = wsh_line_copy(src);
 	wsh_line_ops_smooth(cpy, 4);
 	printf("cpy line for brush has %llu points\n", cpy->num);
@@ -206,43 +205,43 @@ void brsh_brush_update_new_slow(BBrush* brush)
 	WLine* stroke = wsh_line_create();
 	for (int i = 0; i < cpy->num - 1; i++)
 	{
-
+		
 		WPoint p = cpy->data[i];
-
+		
 		x = p.x;
 		y = p.y;
-
+		
 		WPoint np = cpy->data[i + 1];
-
+		
 		nx = np.x;
 		ny = np.y;
-
+		
 		//	first
 		if (i == 0)
 		{
 			wsh_line_add_point(stroke, p);
 		}
-
+		
 		if (i > 0 && i < cpy->num - 1)
 		{
-
+			
 			double lx;
 			double ly;
 			double rx, ry;
-
+			
 			_brush_tangents(brush, p, np, &lx, &ly, &rx, &ry);
-
+			
 			WPoint* l = wsh_point_create_2f(lx, ly);
 			WPoint* r = wsh_point_create_2f(rx, ry);
 			wsh_line_add_point(stroke, *l);
 			wsh_line_add_point(stroke, *r);
 		}
-
+		
 		if (i == cpy->num - 2)
 		{
 			wsh_line_add_point(stroke, p);
 		}
-
+		
 		stroke->closed     = true;
 		stroke->has_fill   = true;
 		stroke->has_stroke = true;
@@ -250,23 +249,23 @@ void brsh_brush_update_new_slow(BBrush* brush)
 		stroke->fill.g     = 0;
 		stroke->fill.b     = 1;
 		stroke->fill.a     = .5;
-
+		
 		if (brush->stroke)
 		{
 			wsh_line_destroy(brush->stroke);
 		}
-
+		
 		brush->stroke = stroke;
 		printf("Created stroke with %llu points\n", stroke->num);
 	}
-
+	
 	brush->needs_update = false;
 }
 
 void brsh_brush_update_old_fast(BBrush* brush)
 {
-
-
+	
+	
 	if (!brush)
 	{
 		printf("oops");
